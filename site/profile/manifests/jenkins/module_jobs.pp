5a7bf9b067caedf428f3a5f228ad54caf0d10c16
Greg Sarjeant <gsarjeant@puppetlabs.com>
2015-01-04 18:21:18 -0600

    Add jobs for gitlab

--- site/profile/manifests/jenkins/module_jobs.pp
+++ site/profile/manifests/jenkins/module_jobs.pp
@@ -18,4 +18,20 @@ define profile::jenkins::module_jobs {
   jenkins::job { "${name}_serverspec":
     config => template("profile/jenkins/serverspec.xml.erb"),
   }
+
+  jenkins::job { "gitlab_${name}_lint":
+    config => template("profile/jenkins/gitlab_lint.xml.erb"),
+  }
+
+  jenkins::job { "$gitlab_{name}_parse":
+    config => template("profile/jenkins/gitlab_parse.xml.erb"),
+  }
+
+  jenkins::job { "gitlab_${name}_rspec":
+    config => template("profile/jenkins/gitlab_rspec.xml.erb"),
+  }
+
+  jenkins::job { "gitlab_${name}_serverspec":
+    config => template("profile/jenkins/gitlab_serverspec.xml.erb"),
+  }
 }
